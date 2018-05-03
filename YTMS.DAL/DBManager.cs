4f930119982d8ecc02d033c9cc3fd4ef89199786
yuanyanjun <281838991@qq.com>
2018-05-03 14:23:18 +0800

    s

---
+++ YTMS.DAL/DBManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SqlSugar;
+namespace YTMS.DAL
+{
+    public class DBManager
+    {
+        public static SqlSugarClient GetInstance()
+        {
+            var db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = SugarDao.ConnectionString,
+                DbType = DbType.MySql
+            });
+            return db;
+        }
+
+        public static SqlSugarClient GetInstance(string connectionKey)
+        {
+            var db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = SugarDao.GetConnectionString(connectionKey),
+                DbType = DbType.MySql
+            });
+            return db;
+        }
+    }
+}
---
+++ YTMS.DAL/SugarDao.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlSugar;
+using System.Configuration;
+
+namespace YTMS.DAL
+{
+    public class SugarDao
+    {
+        //禁止实例化
+        private SugarDao()
+        {
+
+        }
+        public static string ConnectionString
+        {
+            get
+            {
+                string reval = ConfigurationManager.ConnectionStrings["MySQLConn"].ConnectionString; //这里可以动态根据cookies或session实现多库切换
+                return reval;
+            }
+        }
+
+        public static string GetConnectionString(string contactionKey)
+        {
+            if (string.IsNullOrWhiteSpace(contactionKey))
+            {
+
+                return ConnectionString;
+            }
+
+            return ConfigurationManager.ConnectionStrings[contactionKey].ConnectionString;
+        }
+
+        public static SqlSugarClient GetInstance()
+        {
+            var db = new SqlSugarClient(new ConnectionConfig() { ConnectionString = ConnectionString });
+            return db;
+        }
+    }
+}
